37637a2dc326acd22ff1b746fd47a42ea4812add
Rufat Aliyev <rufat845@gmail.com>
2023-05-19 02:12:45 +0400

    explore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,19 @@
-const links = document.querySelectorAll(".header__links li");
+// const links = document.querySelectorAll(".header__links li");
 
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    link.classList.toggle("active");
-  });
-});
-
+// links.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     link.classList.toggle("active");
+//     // link.classList.remove("active")
+//   });
+// });
+var links = document.querySelectorAll(".header__links li");
+for (var i = 0, length = links.length; i < length; i++) {
+  links[i].onclick = function () {
+    var activeLink = document.querySelector(".header__links li.active");
+    if (activeLink) activeLink.classList.remove("active");
+    this.classList.add("active");
+  };
+}
 const menuCloseBtn = document.querySelector("#menuCloseBtn");
 const menuOpenBtn = document.querySelector("#menuOpenBtn");
 const menu = document.querySelector("#menu");
