9cf030dc8399194c758044c521b3980cb68c68bd
Rufat Aliyev <rufat845@gmail.com>
2023-05-22 01:15:43 +0400

    flexy

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,60 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+/* Swiper
+ **************************************************************/
+var swiper = swiper;
+var init = false;
+
+/* Which media query
+ **************************************************************/
+function swiperMode() {
+  let mobile = window.matchMedia("(min-width: 0px) and (max-width: 768px)");
+  let tablet = window.matchMedia("(min-width: 769px) and (max-width: 1024px)");
+  let desktop = window.matchMedia("(min-width: 1025px)");
+
+  // Enable (for mobile)
+  if (mobile.matches) {
+    if (!init) {
+      init = true;
+      swiper = new Swiper(".mySwiper", {
+        loop: true,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+        },
+
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
+    }
+  }
+
+  // Disable (for tablet)
+  else if (tablet.matches) {
+    swiper.destroy();
+    init = false;
+  }
+
+  // Disable (for desktop)
+  else if (desktop.matches) {
+    swiper.destroy();
+    init = false;
+  }
+}
+
+/* On Load
+ **************************************************************/
+window.addEventListener("load", function () {
+  swiperMode();
+});
+
+/* On Resize
+ **************************************************************/
+window.addEventListener("resize", function () {
+  swiperMode();
+});
+
+//
+// const player = videojs('vid1', {});
